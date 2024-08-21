1db2fef3408c43540eddfe754f77abfd619b1db2
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2024-08-21 20:27:02 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+const App = () => {
+  return (
+    <Container>
+      <Typography variant="h1" textAlign={'center'}>Hello, World!</Typography>
+    </Container>
+  );
+};
+
+export default App;
